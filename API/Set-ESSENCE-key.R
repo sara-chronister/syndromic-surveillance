e4c1a04cbb43bc7b16eb2c7933f51c368f06feff
Sara Chronister <sara.chronister@gmail.com>
2021-03-08 11:35:25 -0700

    Update and rename Set-ESSENCE-key to Set-ESSENCE-key.R

--- API/Set-ESSENCE-key.R
+++ API/Set-ESSENCE-key.R
@@ -5,4 +5,5 @@ library(keyring)
 # this must be run at the beginning of each new session
 # REMEMBER TO COMMENT OUT THIS LINE AFTER YOU RUN IT
 # otherwise it will ask for your password every time
-key_set(service = "essence", username = "username01")
+
+key_set(service = "essence", username)
